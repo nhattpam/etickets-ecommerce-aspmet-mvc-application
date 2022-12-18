cf51df16f7eeeb56f7d3b12cdfaabc088bdbd246
Nhat Pham <nhat2up@gmail.com>
2022-12-18 07:35:00 +0700

    controllers

--- eTickets/Program.cs
+++ eTickets/Program.cs
@@ -1,7 +1,12 @@
+using eTickets.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+// Add DbContxt Configuration
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString")));
 
 var app = builder.Build();
 
@@ -23,5 +28,8 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-
+//seed database
+AppDbInitializer.Seed(app);
 app.Run();
+
+
